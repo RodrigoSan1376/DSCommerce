d36a0aa21b852486f46f1011f857e9ff36c93369
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-02-15 10:06:10 -0300

    Access control

--- src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
@@ -3,9 +3,9 @@ package com.devsuperior.dscommerce.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -14,15 +14,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
 	
 	@Autowired
-	private JwtTokenStore tokenStore;
-	
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+	private JwtTokenStore tokenStore;	
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -37,8 +36,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 			http.headers().frameOptions().disable();
 		}
 		
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll()
-		.anyRequest().authenticated();
+		http.authorizeRequests().anyRequest().permitAll();
 	}	
 }
