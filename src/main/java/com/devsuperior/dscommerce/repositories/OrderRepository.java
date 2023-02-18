dd1745bb1712e9148d03f57f44fa3a250b04bc3e
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-02-18 14:02:14 -0300

    Saving Order

--- src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
@@ -4,8 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dscommerce.entities.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long>{
-	
-	
-	
+public interface OrderRepository extends JpaRepository<Order, Long>{	
 }
