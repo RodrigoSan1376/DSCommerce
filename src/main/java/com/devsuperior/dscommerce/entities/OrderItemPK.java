bb719443b2a798f440f1f5af0fad8043a308e7aa
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-02-19 11:32:37 -0300

    Dev profile

--- src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
@@ -37,4 +38,21 @@ public class OrderItemPK implements Serializable{
     public void setProduct(Product product) {
         this.product = product;
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}    
 }
--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -94,4 +95,21 @@ public class Product {
     public List<Order> getOrders() {
         return items.stream().map(x -> x.getOrder()).toList();
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
 }
